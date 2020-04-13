66cf91a0e851cb138e7f63bb95cc82c0d2891084
Laura Osorio <laurisosorio7@hotmail.com>
2020-04-13 05:31:19 -0500

    Instalación

---
+++ finalwork/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Proyecto from "./Pages/Proyecto";
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
